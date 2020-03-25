e7e8953cdfec72bb81ec359851707fe1e3396315
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-24 23:26:26 -0700

    check for last run: small optimizations

--- pythonanywhere_3_months/last_run.py
+++ pythonanywhere_3_months/last_run.py
@@ -5,9 +5,8 @@ from pythonanywhere_3_months import *
 def check():
     """Checks if its been more than 2 months since the script has been run, reports to user on stdout"""
     with open(last_run_at_absolute_path) as f:
-        time_since_run = time() - int(float(f.read().strip()))
-    two_months_in_seconds = 5184000
-    if time_since_run > two_months_in_seconds:
-        print("Its been more than 2 months since you've ran 'pythonanywhere_3_months'!")
-        sys.exit(1)
+        # if last time this ran was more than 2 months ago
+        if time() - float(f.read().strip()) > 5184000:
+            print("Its been more than 2 months since you've ran 'pythonanywhere_3_months'!")
+            sys.exit(1)
     sys.exit(0)
